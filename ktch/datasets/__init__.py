ff66eba4b14670e7b2777ccc6aad6371fd443205
Noshita, Koji <noshita@morphometrics.jp>
2022-08-05 00:21:22 +0900

    ✨ NEW: add data loaders for mosquito wings

---
+++ ktch/datasets/__init__.py
@@ -0,0 +1,7 @@
+"""
+The :mod:`ktch.datasets` module implements utility functions to load morphometric datasets.
+"""
+
+from ._base import load_landmark_mosquito_wings, load_outline_mosquito_wings
+
+__all__ = ["load_landmark_mosquito_wings", "load_outline_mosquito_wings"]
