ca4896ab1b85c01c96a93a0a852133876fbf6715
Koji Noshita <noshita@morphometrics.jp>
2023-06-23 10:21:54 +0900

    🔧 MAINTAIN: update .gitignore
    🔧 MAINTAIN: update .gitignore

---
+++ ktch/motion/__init__.py
@@ -0,0 +1,24 @@
+"""
+The :mod:`katatch.motion` module implements utility functions for analyzing motion (time-series of morphological properties).
+
+"""
+
+# Copyright 2022 Koji Noshita
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# from ._matching import match_landmarks
+
+__all__ = [
+    # "match_landmarks",
+]
