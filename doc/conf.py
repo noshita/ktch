ceb1ee310ce9b00f13b836592ea73a39074b43a9
Koji Noshita <noshita@morphometrics.jp>
2023-07-06 16:14:29 +0900

    docs: 📚 doc

--- doc/conf.py
+++ doc/conf.py
@@ -125,7 +125,7 @@ release = __version__
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
-exclude_patterns = ["_build", "_templates"]
+exclude_patterns = ["_build", "_templates", "README.md"]
 
 # The reST default role (used for this markup: `text`) to use for all
 # documents.
