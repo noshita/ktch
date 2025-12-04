543c1e98464bbd541beb42e7b9965e66fa10251b
Noshita, Koji <noshita@morphometrics.jp>
2025-12-04 17:32:49 +0900

    fix: 🐛 tests for io of SPHARM-PDM

--- ktch/io/tests/test_SPHARM_PDM.py
+++ ktch/io/tests/test_SPHARM_PDM.py
@@ -4,29 +4,35 @@ from pathlib import Path
 import pytest
 from numpy.testing import assert_array_almost_equal
 
-from ktch.io import read_coef_SPHARM_PDM
+from ktch.io import read_spharmpdm_coef
 
 
-def test_read_coef_SPHARM_PDM_m_0():
+def test_read_spharmpdm_coef_valid_shape():
+    path = Path(__file__).parent / "data" / "andesred_07_allSegments_SPHARM.coef"
+
+    coef_list = read_spharmpdm_coef(path)
+
+    assert len(coef_list) > 0
+    for l, coef in enumerate(coef_list):
+        assert coef.shape == (2 * l + 1, 3)
+
+
+def test_read_spharmpdm_coef_m_0():
     path = Path(__file__).parent / "data" / "andesred_07_allSegments_SPHARM.coef"
 
     with open(path, "r") as f:
         coef_txt = f.read()
     coef_list_ = ast.literal_eval(coef_txt.replace("{", "[").replace("}", "]"))
-    coef_list = coef_list_[1:]
+    coef_list_raw = coef_list_[1:]
 
     l_max = int((coef_list_[0]) ** 0.5 - 1)
 
-    coef_x, coef_y, coef_z = read_coef_SPHARM_PDM(path)
+    coef_list = read_spharmpdm_coef(path)
 
-    assert coef_x.n_degree == l_max
-    assert coef_y.n_degree == l_max
-    assert coef_z.n_degree == l_max
+    assert len(coef_list) - 1 == l_max
 
-    assert coef_x[0] == coef_list[0][0]
-    assert coef_y[0] == coef_list[0][1]
-    assert coef_z[0] == coef_list[0][2]
+    assert coef_list[0][0, 0] == coef_list_raw[0][0]
+    assert coef_list[0][0, 1] == coef_list_raw[0][1]
+    assert coef_list[0][0, 2] == coef_list_raw[0][2]
     for l in range(1, l_max + 1):
-        assert coef_x[l, 0] == coef_list[l**2][0]
-        assert coef_y[l, 0] == coef_list[l**2][1]
-        assert coef_z[l, 0] == coef_list[l**2][2]
+        assert_array_almost_equal(coef_list[l][l], coef_list_raw[l**2])
